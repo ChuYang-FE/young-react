de4e487606f8a34da2408e89277752f66ed5cd3a
young <865428527@qq.com>
2020-08-26 17:49:24 +0800

    React哲学-实现商品搜索功能-step1-静态

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import Welcome from './Welcome';
+import FilterableProductTable from './Components/GoodsSearching/FilterableProductTable';
 import * as serviceWorker from './serviceWorker';
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 ReactDOM.render(
   <React.StrictMode>
     <Welcome />
     <App />
+    <FilterableProductTable className="alert alert-success" products={PRODUCTS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
