adbfe4dc9180909d44d243eb689cc46b7ab5de51
young <865428527@qq.com>
2020-09-12 23:08:19 +0800

    feat 开发环境下跨域：设置package.json的代理地址

--- src/Fetch/Fetch.jsx
+++ src/Fetch/Fetch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import qs from 'querystring';
 
 class Fetch extends React.Component {
   state = { 
@@ -6,12 +7,21 @@ class Fetch extends React.Component {
   }
 
   componentDidMount() {
+    // github
     fetch('https://api.github.com/users/CoderTyy/repos')
 		.then(res => res.json())
     .then(data => {
       this.setState({
         repoArr: data,
       })
+    });
+
+    //音乐
+    //跨域情况，生产模式：解决方案1. package.json文件设置代理
+    fetch('/v1/restserver/ting?method=baidu.ting.billboard.billList&type=1&size=10&offset=0')
+		.then(res => res.json())
+    .then(data => {
+      console.log(data);
     });
   }
 
