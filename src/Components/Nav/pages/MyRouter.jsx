014d8c6eb60a7f9866e13e4c8823b2b10e1d1b29
young <865428527@qq.com>
2020-09-16 23:27:49 +0800

    test reset my user e-mail

--- src/Components/Nav/pages/MyRouter.jsx
+++ src/Components/Nav/pages/MyRouter.jsx
@@ -10,7 +10,6 @@ const MyRouter = props => {
       <div>
         <h2>This is my <span style={{color:'red'}}>Router页面内容</span></h2>
         <button onClick={ handleClick }>跳转到home页</button>
-        {/* 以下<TestBtn />组件没有直接被管理，打印props是空对象，解决方案，以下组件使用withRouter */}
         <TestBtn />
       </div>
   )
