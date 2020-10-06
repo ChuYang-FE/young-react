25f5d0ba80bdbcf73f2e2ea9285bc312b955df3c
young <865428527@qq.com>
2020-10-06 16:57:12 +0800

    testing git revert/reset

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import Welcome from './Welcome';
-import FilterableProductTable from './Components/GoodsSearching/FilterableProductTable';
-import Game from './Components/TicTacToe';
-import * as serviceWorker from './serviceWorker';
-import Fetch from './Fetch/Fetch';
-import NavAndShow from './Components/Nav/NavAndShow';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import Welcome from "./Welcome";
+import FilterableProductTable from "./Components/GoodsSearching/FilterableProductTable";
+import Game from "./Components/TicTacToe";
+import * as serviceWorker from "./serviceWorker";
+import Fetch from "./Fetch/Fetch";
+import NavAndShow from "./Components/Nav/NavAndShow";
 import FirstHook from "./Hook/FirstHook.jsx";
 import RandomUser from "./Hook/UseStateAndUseEffect/RandomUser.jsx";
 import TokenApp from "./Hook/UseStateAndUseEffect/Token.jsx";
@@ -17,15 +17,44 @@ import HookFetch from "./Hook/Fetch/Fetch";
 import NextPage from "./Hook/Fetch/NextPage";
 import UseCallBack from "./Hook/OtherHook/UseCallBack";
 
-
 const PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
-  {category: 'Electronics', price: '$299.99', stocked: true, name: 'Beats 11'}
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+  {
+    category: "Electronics",
+    price: "$299.99",
+    stocked: true,
+    name: "Beats 11",
+  },
 ];
 
 ReactDOM.render(
